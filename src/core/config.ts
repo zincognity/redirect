21b42a3c1402c9d42d33d6ea9065d9dea3a98b15
zincognity <67128655+zincognity@users.noreply.github.com>
2025-06-08 21:52:52 -0500

    feat: add AUTH_CODE to environment variable exports in config

--- src/core/config.ts
+++ src/core/config.ts
@@ -2,6 +2,7 @@ export const {
     CLOUDFLARE_TOKEN: token,
     CLOUDFLARE_EMAIL: email,
     ZONE_ID: zoneId,
+    AUTH_CODE: authCode,
     SITE: site,
     PUBLIC_URL: pageUrl,
 } = import.meta.env;
