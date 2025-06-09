a45bacd6ddbe9f14692801919cc21dcc9dbeabc5
zincognity <67128655+zincognity@users.noreply.github.com>
2025-06-08 20:16:48 -0500

    refactor: Code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 import { site } from "./src/core/config";
 
 // https://astro.build/config
 export default defineConfig({
     site: site,
+    integrations: [react()],
     vite: {
         plugins: [tailwindcss()],
     },
